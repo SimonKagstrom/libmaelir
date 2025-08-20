42a85bdcf32d91a895390ed09db817bc9d42ad45
Simon Kagstrom <simon.kagstrom@profoto.com>
2025-08-20 06:17:29 +0200

    painter: Move here from maelir

---
+++ src/painter/include/painter.hh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "image.hh"
+
+namespace painter
+{
+
+struct Rect
+{
+    int32_t x;
+    int32_t y;
+    int32_t width;
+    int32_t height;
+};
+
+
+void Blit(uint16_t* frame_buffer, const Image& image, Rect to);
+
+void ZoomedBlit(
+    uint16_t* frame_buffer, uint32_t buffer_width, const Image& image, unsigned factor, Rect to);
+
+} // namespace painter
