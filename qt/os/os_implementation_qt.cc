596ea9b02aa2f70eb7b33602a35b18a7c8a5a876
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-04-05 09:57:37 +0200

    qt: Merge all os stuff into os_implementation_qt.cc

--- qt/os/os_implementation_qt.cc
+++ qt/os/os_implementation_qt.cc
@@ -1,15 +1,21 @@
 #include "os_implementation.hh"
 #include "semaphore.hh"
+#include "time.hh"
+
+#include <QThread>
+
+
+using namespace os;
 
 namespace os
 {
-
 struct ThreadContext
 {
-    binary_semaphore suspend_sem {0};
+    QThread* m_thread;
 };
 
 } // namespace os
+
 
 os::ThreadHandle
 os::detail::GetCurrentThread()
@@ -19,14 +25,49 @@ os::detail::GetCurrentThread()
     return &context;
 }
 
-void
-os::detail::AwakeThread(os::ThreadHandle thread)
+os::ThreadHandle
+os::detail::CreateThread(const std::function<void()>& thread_loop)
 {
-    thread->suspend_sem.release();
+    auto out = new ThreadContext;
+    out->m_thread = QThread::create([thread_loop]() { thread_loop(); });
+
+    return out;
 }
 
 void
-os::detail::SuspendThread(os::ThreadHandle thread)
+os::detail::WaitThreadExit(ThreadHandle thread)
 {
-    thread->suspend_sem.acquire();
+    thread->m_thread->wait();
+
+    delete thread;
+}
+
+void
+os::detail::StartThread(ThreadHandle thread, const char* name, ThreadCore, ThreadPriority, uint32_t)
+{
+    auto qt_thread = thread->m_thread;
+    qt_thread->setObjectName(name);
+    qt_thread->start();
+}
+
+
+milliseconds
+os::GetTimeStamp()
+{
+    static auto at_start = std::chrono::high_resolution_clock::now();
+
+    return std::chrono::duration_cast<std::chrono::milliseconds>(
+        std::chrono::high_resolution_clock::now() - at_start);
+}
+
+uint32_t
+os::GetTimeStampRaw()
+{
+    return GetTimeStamp().count();
+}
+
+void
+os::Sleep(milliseconds delay)
+{
+    QThread::msleep(delay.count());
 }
