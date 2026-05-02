fb2111c6640af1b230cf8b1d77463884d31eac9a
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-05-02 09:38:34 +0200

    i_nvm: Correct gcc build

--- src/include/hal/i_nvm.hh
+++ src/include/hal/i_nvm.hh
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <cstdint>
+#include <cstring>
 #include <optional>
 #include <string>
 #include <string_view>
+#include <type_traits>
 
 namespace hal
 {
@@ -16,42 +18,48 @@ public:
     template <typename T>
     std::optional<T> Get(const char* key)
     {
-        static_assert(sizeof(T) <= sizeof(uint32_t), "T must fit in a uint32_t");
-
-        if (auto raw = GetUint32_t(key); raw)
+        if constexpr (std::is_same_v<T, std::string>)
         {
-            T value = {};
-
-            memcpy(&value, &*raw, sizeof(T));
-
-            return value;
+            return GetString(key);
         }
+        else
+        {
+            static_assert(sizeof(T) <= sizeof(uint32_t), "T must fit in a uint32_t");
+            static_assert(std::is_trivially_copyable_v<T>,
+                          "T must be trivially copyable for INvm::Get");
 
-        return std::nullopt;
-    }
+            if (auto raw = GetUint32_t(key); raw)
+            {
+                T value = {};
 
-    template <>
-    std::optional<std::string> Get<std::string>(const char* key)
-    {
-        return GetString(key);
+                memcpy(&value, &*raw, sizeof(T));
+
+                return value;
+            }
+
+            return std::nullopt;
+        }
     }
 
     template <typename T>
-    void Set(const char* key, const T &value)
+    void Set(const char* key, const T& value)
     {
-        static_assert(sizeof(T) <= sizeof(uint32_t), "T must fit in a uint32_t");
+        if constexpr (std::is_same_v<T, std::string>)
+        {
+            SetString(key, value);
+        }
+        else
+        {
+            static_assert(sizeof(T) <= sizeof(uint32_t), "T must fit in a uint32_t");
+            static_assert(std::is_trivially_copyable_v<T>,
+                          "T must be trivially copyable for INvm::Set");
 
-        uint32_t raw = 0;
+            uint32_t raw = 0;
 
-        memcpy(&raw, &value, sizeof(T));
+            memcpy(&raw, &value, sizeof(T));
 
-        SetUint32_t(key, raw);
-    }
-
-    template <>
-    void Set<std::string>(const char* key, const std::string& value)
-    {
-        SetString(key, value);
+            SetUint32_t(key, raw);
+        }
     }
 
     virtual void Commit() = 0;
