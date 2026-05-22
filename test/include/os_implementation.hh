7e0e3b09d6acee48bf76db48dab6f460f6a52ec6
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-05-22 07:27:44 +0200

    unittest: Add AllocFast/SlowMem to the os

--- test/include/os_implementation.hh
+++ test/include/os_implementation.hh
@@ -26,6 +26,8 @@ public:
 
 namespace detail
 {
+template <typename T>
+using mem_unique_ptr = std::unique_ptr<T, void (*)(T*)>;
 
 ThreadHandle GetCurrentThread();
 
@@ -45,6 +47,38 @@ void WaitThreadExit(ThreadHandle thread);
 // Unit test only
 void SetCurrentThread(ThreadHandle thread);
 std::shared_ptr<MockKernel> GetKernelMock();
+
+template <typename T>
+inline mem_unique_ptr<T>
+AllocFastMem(unsigned alignment [[maybe_unused]])
+{
+    const size_t alloc_alignment = alignment > alignof(T) ? alignment : alignof(T);
+    auto deleter = +[](T* ptr) {
+        if (ptr != nullptr)
+        {
+            ptr->~T();
+            std::free(ptr);
+        }
+    };
+
+    void* raw_ptr = nullptr;
+    if (posix_memalign(&raw_ptr, alloc_alignment, sizeof(T)) != 0)
+    {
+        return mem_unique_ptr<T>(nullptr, deleter);
+    }
+
+    auto* object = new (raw_ptr) T();
+    return mem_unique_ptr<T>(object, deleter);
+}
+
+template <typename T>
+inline mem_unique_ptr<T>
+AllocSlowMem(unsigned alignment)
+{
+    // Same on Qt
+    return detail::AllocFastMem<T>(alignment);
+}
+
 
 } // namespace detail
 
