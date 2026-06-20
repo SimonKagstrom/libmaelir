24ddb4d91ec452addee97cdaa114950344ecaf9b
Simon Kagstrom <simon.kagstrom@gmail.com>
2026-06-20 15:07:38 +0200

    touch_esp32: Allow interrupts (hack for now)

--- esp32/touch_esp32/include/touch_esp32.hh
+++ esp32/touch_esp32/include/touch_esp32.hh
@@ -2,20 +2,24 @@
 
 #include "hal/i_touch.hh"
 
+#include <driver/i2c_master.h>
 #include <esp_lcd_touch.h>
+#include <esp_lcd_touch_gt911.h>
 #include <etl/vector.h>
 
 class TouchEsp32 : public hal::ITouch
 {
 public:
-    explicit TouchEsp32(esp_lcd_touch_handle_t tp);
+    explicit TouchEsp32(const i2c_master_bus_config_t& i2c_mst_config, gpio_num_t interrupt_pin);
 
 private:
     std::unique_ptr<ListenerCookie> AttachIrqListener(std::function<void()> on_state_changed) final;
 
     std::span<const hal::ITouch::Data> GetActiveTouchData() final;
 
+    const gpio_num_t m_interrupt_pin;
     esp_lcd_touch_handle_t m_tp;
+    std::function<void()> m_on_state_changed_callback {[]() {}};
     etl::vector<hal::ITouch::Data, 10> m_touch_data_buffer;
     bool m_was_pressed {false};
     uint16_t m_last_x {0};
--- esp32/touch_esp32/touch_esp32.cc
+++ esp32/touch_esp32/touch_esp32.cc
@@ -2,15 +2,71 @@
 
 #include "i_display_properties.hh"
 
-TouchEsp32::TouchEsp32(esp_lcd_touch_handle_t tp)
-    : m_tp(tp)
+TouchEsp32::TouchEsp32(const i2c_master_bus_config_t &i2c_mst_config, gpio_num_t interrupt_pin)
+    : m_interrupt_pin(interrupt_pin)
 {
+    // TODO: All this is hardcoded for this particular case...
+    i2c_master_bus_handle_t i2c_handle;
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_mst_config, &i2c_handle));
+
+    esp_lcd_panel_io_i2c_config_t io_config = {.dev_addr = ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS,
+                                               .scl_speed_hz = 100000,
+                                               .control_phase_bytes = 1,
+                                               .dc_bit_offset = 0,
+                                               .lcd_cmd_bits = 16,
+                                               .lcd_param_bits = 0,
+                                               .on_color_trans_done = nullptr,
+                                               .user_ctx = nullptr,
+                                               .flags = {
+                                                   .dc_low_on_data = 0,
+                                                   .disable_control_phase = 1,
+                                               }};
+
+    esp_lcd_touch_io_gt911_config_t tp_gt911_config = {
+        .dev_addr = static_cast<uint8_t>(io_config.dev_addr),
+    };
+
+    const esp_lcd_touch_config_t tp_cfg = {
+        .x_max = hal::kDisplayWidth,
+        .y_max = hal::kDisplayHeight,
+        .rst_gpio_num = GPIO_NUM_NC,
+        .int_gpio_num = m_interrupt_pin,
+        .levels =
+            {
+                .reset = 0,
+                .interrupt = 0,
+            },
+        .flags =
+            {
+                .swap_xy = 1,
+                .mirror_x = 0,
+                .mirror_y = 1,
+            },
+        .process_coordinates = nullptr,
+        .interrupt_callback =
+            [](esp_lcd_touch_handle_t tp) {
+                auto p = static_cast<TouchEsp32*>(tp->config.user_data);
+
+                p->m_on_state_changed_callback();
+            },
+        .user_data = this,
+        .driver_data = &tp_gt911_config,
+    };
+    esp_lcd_panel_io_handle_t tp_io_handle = nullptr;
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_handle, &io_config, &tp_io_handle));
+    ESP_ERROR_CHECK(esp_lcd_touch_new_i2c_gt911(tp_io_handle, &tp_cfg, &m_tp));
 }
 
 std::unique_ptr<ListenerCookie>
 TouchEsp32::AttachIrqListener(std::function<void()> on_state_changed)
 {
-    return nullptr;
+    if (m_interrupt_pin == GPIO_NUM_NC)
+    {
+        return nullptr;
+    }
+
+    m_on_state_changed_callback = std::move(on_state_changed);
+    return std::make_unique<ListenerCookie>([this]() { m_on_state_changed_callback = []() {}; });
 }
 
 std::span<const hal::ITouch::Data>
